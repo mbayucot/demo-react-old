8ccc57b562a91b5865c3807150f6422f36996c0b
Michael Bayucot <mbayucot@gmail.com>
2021-10-02 20:41:45 +0800

    Add home and dashboard

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -5,6 +5,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 const Home = lazy(() => import('./pages/HomePage'));
 const Login = lazy(() => import('./user/SignIn/SignInPage'));
 const Register = lazy(() => import('./user/SignUp/SignUpPage'));
+const Dashboard = lazy(() => import('./pages/DashboardPage'));
 const NoMatch = lazy(() => import('./pages/NoMatchPage'));
 
 const App = (): ReactElement => {
@@ -21,6 +22,10 @@ const App = (): ReactElement => {
 
         <Route path="/register">
           <Register />
+        </Route>
+
+        <Route path="/dashboard">
+          <Dashboard />
         </Route>
 
         <Route path="*">
--- packages/client/src/pages/HomePage.tsx
+++ packages/client/src/pages/HomePage.tsx
@@ -1,7 +1,90 @@
 import React, { FC } from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Link from '@mui/material/Link';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 const HomePage: FC = () => {
-  return <p>Home page</p>;
+  return (
+    <React.Fragment>
+      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <CssBaseline />
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+            Demo
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
+          </Search>
+          <nav>
+            <Link variant="button" color="text.primary" href="#" sx={{ my: 1, mx: 1.5 }}>
+              Sign in
+            </Link>
+          </nav>
+          <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+            Sign up
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
 };
 
 export default HomePage;
