784ee74ac20db04544edeb28a3a0140b50075368
Michael Bayucot <mbayucot@gmail.com>
2021-10-05 21:10:25 +0800

    Add admin

---
+++ packages/admin/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+const App = (): ReactElement => {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+    </div>
+  );
+};
+
+export default App;
