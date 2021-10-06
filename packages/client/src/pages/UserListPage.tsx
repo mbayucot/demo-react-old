aff8c2f62768b1589e79cf679f508a51cdf01f09
Michael Bayucot <mbayucot@gmail.com>
2021-10-06 20:14:03 +0800

    Add query

--- packages/client/src/pages/UserListPage.tsx
+++ packages/client/src/pages/UserListPage.tsx
@@ -1,11 +1,27 @@
 import React, { FC } from 'react';
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridToolbarContainer, GridToolbarExport } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+import { gql, useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
 
-const rows: GridRowsProp = [
-  { id: 1, email: 'demos@softaculous.com', name: 'test test', role: 'Administrator', posts: '1' },
-  { id: 2, email: 'demos@softaculous.com', name: 'test test', role: 'Author', posts: '1' },
-  { id: 3, email: 'demos@softaculous.com', name: 'test test', role: 'Author', posts: '1' },
-];
+const GET_USERS = gql`
+  query GetUsers {
+    users(page: 1, limit: 10) {
+      collection {
+        id
+        email
+        firstName
+        lastName
+      }
+      metadata {
+        totalPages
+        totalCount
+        currentPage
+        limitValue
+      }
+    }
+  }
+`;
 
 const columns: GridColDef[] = [
   { field: 'email', headerName: 'Email' },
@@ -14,10 +30,39 @@ const columns: GridColDef[] = [
   { field: 'posts', headerName: 'Posts' },
 ];
 
+const CustomToolbar: FC = () => {
+  const onAddNewClick = () => {
+    console.log('onAddNewClick');
+  };
+
+  return (
+    <GridToolbarContainer>
+      <Link to={'/users/new'}>Add New</Link>
+      <Button variant="contained" onClick={onAddNewClick}>
+        Add New
+      </Button>
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+};
+
 const UserListPage: FC = () => {
+  const { loading, error, data } = useQuery(GET_USERS);
+
+  if (loading) return <p>'Loading...'</p>;
+  if (error) return <p>`Error! ${error.message}`</p>;
+
   return (
     <div style={{ height: 300, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} loading={false} hideFooterSelectedRowCount={true} />
+      <DataGrid
+        rows={data.users.collection}
+        columns={columns}
+        loading={false}
+        hideFooterSelectedRowCount={true}
+        components={{
+          Toolbar: CustomToolbar,
+        }}
+      />
     </div>
   );
 };
