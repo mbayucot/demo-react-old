dfd98502cf3d4e58689b2542426b9bcdf97707ea
Michael Bayucot <mbayucot@gmail.com>
2021-10-06 21:22:58 +0800

    Add edit and update

--- packages/client/src/pages/User/EditUserPage.tsx
+++ packages/client/src/pages/User/EditUserPage.tsx
@@ -4,36 +4,92 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import UserForm from '@demo/client/src/pages/User/UserForm';
 import { useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 
 import { LoginFormValues, validationSchema } from '@demo/client/src/pages/User/UserForm';
+import { CREATE_USER } from './NewUserPage';
 
 type Params = {
   id: string;
 };
 
+type UserAttributes = {
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  password?: string;
+};
+
+const GET_USER = gql`
+  query GetUser($id: ID!) {
+    user(id: $id) {
+      id
+      email
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const UPDATE_USER = gql`
+  mutation UpdateUser($id: ID!, $attributes: UserAttributes!) {
+    updateUser(id: $id, attributes: $attributes) {
+      user {
+        id
+        email
+      }
+    }
+  }
+`;
+
 const EditUserPage: FC = () => {
   let { id } = useParams<Params>();
 
-  const EnhancedLoginForm = withFormik<{}, LoginFormValues>({
-    mapPropsToValues: () => ({
-      email: '',
-      first_name: '',
-      last_name: '',
-      password: '',
-      role: '',
+  const { loading, error, data } = useQuery(GET_USER, {
+    variables: { id: id },
+  });
+
+  const [updateUser] = useMutation(UPDATE_USER);
+
+  if (loading) return <p>'Loading...'</p>;
+  if (error) return <p>`Error! ${error.message}`</p>;
+
+  const EnhancedLoginForm = withFormik<
+    {
+      lastName: string;
+      firstName: string;
+      email: string;
+      password: string;
+    },
+    LoginFormValues
+  >({
+    mapPropsToValues: (props) => ({
+      email: props.email || '',
+      first_name: props.firstName || '',
+      last_name: props.lastName || '',
+      password: props.password || '',
     }),
 
     validationSchema: validationSchema,
 
     handleSubmit: async (values: LoginFormValues, { props, ...actions }) => {
-      console.log('here');
+      await updateUser({
+        variables: {
+          id: id,
+          attributes: {
+            firstName: values.first_name,
+            lastName: values.last_name,
+          },
+        },
+      });
     },
   })(UserForm);
 
   return (
     <Container>
       <Box>
-        <EnhancedLoginForm />
+        <EnhancedLoginForm {...data.user} />
       </Box>
     </Container>
   );
