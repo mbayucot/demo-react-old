4da3de7551d305c8e4e375f92fe22e5f2e7f2bfc
Michael Bayucot <mbayucot@gmail.com>
2021-11-24 20:39:20 +0800

    Add tests to SignInpage

---
+++ packages/client/src/user/__mocks__/auth.ts
@@ -0,0 +1,41 @@
+import { rest } from 'msw';
+
+interface User {
+  id: number;
+  email: string;
+}
+
+const data = {
+  id: 1,
+  email: 'test@test.com',
+} as User;
+
+const token = '__test_token__';
+
+const handlers = [
+  rest.post(`/signup`, (req, res, ctx) => {
+    const { user } = req.body as {
+      user: User;
+    };
+    if (user.email === 'invalid@email.com') {
+      return res(ctx.status(422), ctx.json({ email: '__test_error_description__' }));
+    }
+
+    return res(ctx.status(200), ctx.set('authorization', token), ctx.json(data));
+  }),
+  rest.post(`http://localhost:4000/login`, (req, res, ctx) => {
+    const { user } = req.body as {
+      user: User;
+    };
+    if (user.email === 'invalid@email.com') {
+      return res(ctx.status(422), ctx.json({ email: '__test_error_description__' }));
+    }
+
+    return res(ctx.status(200), ctx.set('authorization', token), ctx.json(data));
+  }),
+  rest.delete(`/logout`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
+
+export { handlers, data };
---
+++ packages/client/src/user/__tests__/SignIn/SignInPageTest.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { render, screen, act, waitFor, fireEvent, waitForElementToBeRemoved, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ApolloProvider } from '@apollo/client';
+import { setupServer } from 'msw/node';
+import faker from 'faker';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../../../app/store';
+
+import { client } from '../../../client';
+import SignInPage from '../../SignIn/SignInPage';
+import '../../../mockedGraphQLServer';
+
+import { handlers } from '../../__mocks__/auth';
+
+const setup = () => {
+  const utils = render(
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <ApolloProvider client={client}>
+          <MemoryRouter initialEntries={['/sigin']}>
+            <SignInPage />
+            <Route path="/dashboard">
+              <div>Dashboard</div>
+            </Route>
+          </MemoryRouter>
+        </ApolloProvider>
+      </Provider>
+    </PersistGate>,
+  );
+  const changeEmailInput = (value: string) => userEvent.type(utils.getByLabelText(/email/i), value);
+  const changePasswordInput = (value: string) => userEvent.type(utils.getByLabelText(/password/i), value);
+  utils.debug(undefined, 300000);
+  const submitButton = screen.getByRole('button', {
+    name: /sign in/i,
+  });
+  const clickSubmit = () => userEvent.click(submitButton);
+  return {
+    utils,
+    changeEmailInput,
+    changePasswordInput,
+    submitButton,
+    clickSubmit,
+  };
+};
+
+describe('ProfilePage', () => {
+  const server = setupServer(...handlers);
+
+  beforeAll(() => server.listen());
+
+  afterEach(() => {
+    server.resetHandlers();
+  });
+
+  afterAll(() => server.close());
+
+  it('should show validation errors', async () => {
+    const { utils, changeEmailInput, changePasswordInput, clickSubmit } = setup();
+    await act(async () => {
+      await clickSubmit();
+    });
+    expect(await screen.findByText(/email is required/i)).toBeInTheDocument();
+    expect(await screen.findByText(/password is required/i)).toBeInTheDocument();
+  });
+
+  it('should redirect to posts page after saving', async () => {
+    const { utils, changeEmailInput, changePasswordInput, clickSubmit } = setup();
+    changeEmailInput(faker.internet.email());
+    changePasswordInput(faker.random.word());
+    await clickSubmit();
+    await waitForElementToBeRemoved(() => utils.queryByText('Loading...'));
+    await waitFor(() => utils.findByText(/dashboard/i));
+  });
+});
